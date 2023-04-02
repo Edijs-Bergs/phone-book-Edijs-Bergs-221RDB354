b4823f1b068814668b0b2538493a22b0738054d7
Edijs-Bergs <90458087+Edijs-Bergs@users.noreply.github.com>
2023-04-02 18:08:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,66 @@
-# python3
+import random
 
-class Query:
-    def __init__(self, query):
-        self.type = query[0]
-        self.number = int(query[1])
-        if self.type == 'add':
-            self.name = query[2]
+class HashTable:
+    def __init__(self):
+        # Initialize the hash table with 500 buckets, a random prime number,
+        # and an arbitrary multiplier value
+        self.bucket_count = 500
+        self._multiplier = 1234
+        self._prime = random.randint(0,200)
+        self.buckets = [[] for _ in range(self.bucket_count)]
 
-def read_queries():
-    n = int(input())
-    return [Query(input().split()) for i in range(n)]
+    def _hash_func(self, query):
+        # Private method that takes a string and returns a hash value
+        ans = 0
+        for c in reversed(query):
+            ans = (ans * self._multiplier + ord(c) % self._prime)
+        return ans % self.bucket_count
 
-def write_responses(result):
-    print('\n'.join(result))
+    def add(self, string, string2):
+        # Method that adds a key-value pair to the hash table
+        hashed = self._hash_func(string)
+        bucket = self.buckets[hashed]
+        # If the key is already present in the bucket, update the value
+        for i in range(len(bucket)):
+            if bucket[i][0] == string:
+                bucket[i] = (string, string2)
+                return
+        # Otherwise, append the new key-value pair to the bucket
+        bucket.append((string, string2))
 
-def process_queries(queries):
-    result = []
-    # Keep list of all existing (i.e. not deleted yet) contacts.
-    contacts = []
-    for cur_query in queries:
-        if cur_query.type == 'add':
-            # if we already have contact with such number,
-            # we should rewrite contact's name
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    contact.name = cur_query.name
-                    break
-            else: # otherwise, just add it
-                contacts.append(cur_query)
-        elif cur_query.type == 'del':
-            for j in range(len(contacts)):
-                if contacts[j].number == cur_query.number:
-                    contacts.pop(j)
-                    break
-        else:
-            response = 'not found'
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    response = contact.name
-                    break
-            result.append(response)
-    return result
+    def delete(self, string):
+        # Method that deletes a key-value pair from the hash table
+        hashed = self._hash_func(string)
+        bucket = self.buckets[hashed]
+        # Search for the key in the bucket, and remove it if found
+        for i in range(len(bucket)):
+            if bucket[i][0] == string:
+                bucket.pop(i)
+                break
+
+    def find(self, string):
+        # Method that finds the value associated with a given key
+        hashed = self._hash_func(string)
+        # Search for the key in the appropriate bucket, and return the value if found
+        for elem in self.buckets[hashed]:
+            if elem[0] == string:
+                return elem[1]
+        # Return "not found" if the key is not found in the hash table
+        return "not found"
 
 if __name__ == '__main__':
-    write_responses(process_queries(read_queries()))
-
+    # Main function that reads input queries and executes them on the hash table
+    result = []
+    n = int(input())
+    hash_table = HashTable()  
+    for query in range(n):
+        query = input().split()
+        if query[0] == 'add':
+            hash_table.add(query[1], query[2])
+        elif query[0] == 'find':
+            result.append(hash_table.find(query[1]))
+        elif query[0] =='del':
+            hash_table.delete(query[1])
+    # Print the results of the find queries
+    for rez in result:
+        print(rez)
