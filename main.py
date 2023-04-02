ad41c421cb545ad4456ebdd78acfa19823704c57
Edijs-Bergs <90458087+Edijs-Bergs@users.noreply.github.com>
2023-04-02 18:01:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
-# python3
-
 class Query:
     def __init__(self, query):
         self.type = query[0]
         self.number = int(query[1])
         if self.type == 'add':
             self.name = query[2]
-
 def read_queries():
     n = int(input())
     return [Query(input().split()) for i in range(n)]
-
 def write_responses(result):
     print('\n'.join(result))
-
 def process_queries(queries):
     result = []
     # Keep list of all existing (i.e. not deleted yet) contacts.
@@ -41,7 +36,5 @@ def process_queries(queries):
                     break
             result.append(response)
     return result
-
 if __name__ == '__main__':
     write_responses(process_queries(read_queries()))
-
